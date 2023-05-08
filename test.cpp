cad9fa18218ceef26359a49b0855ab608197c16e
aksharpadman <aksharpadman@gmail.com>
2023-05-08 14:15:50 +0530

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <list>
+#include<queue>
+#include "pqueue.h"
+
+using namespace std;
+
+
+int main()
+{   
+;
+    pqueue frontier;
+    frontier["A"]=2;
+
+   
+}
